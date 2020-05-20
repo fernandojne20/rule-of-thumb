a257d6b9f4af88521d8a23f7648c20c980c5f5c7
Fernando Nuñez <fernandojne.20@gmail.com>
2020-05-19 19:57:20 -0500

    was added the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/shared/header';
 
 const App = () => {
   return (
     <>
-      <h1>start layout</h1>
+      <Header/>
     </>
   );
 }
